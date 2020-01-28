36961cbdad7b9fc7b9b62d9068343d3868983563
Itstep <seleznev_k@itstep.academy>
2020-01-28 19:11:50 +0300

    Add JavaDoc

--- src/org/itstep/msk/Main.java
+++ src/org/itstep/msk/Main.java
@@ -1,6 +1,13 @@
 package org.itstep.msk;
 
+/**
+ * Главный класс проекта
+ */
 public class Main {
+    /**
+     * Главный метод проекта - точка входа
+     * @param args Аргументы программы
+     */
     public static void main(String[] args) {
 	// write your code here
     }
